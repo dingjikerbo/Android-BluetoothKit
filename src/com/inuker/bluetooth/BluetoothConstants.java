5e1f563e99b052de359850e066ac8f5c815fb3a9
liwentian <liwentian@xiaomi.com>
2015-11-18 10:36:54 +0800

    删除旧的代码
    Signed-off-by: liwentian <liwentian@xiaomi.com>

--- src/com/inuker/bluetooth/BluetoothConstants.java
+++
@@ -1,9 +0,0 @@
-package com.inuker.bluetooth;
-
-public class BluetoothConstants {
-
-	public static final int DEFAULT_DURATION = 10000;
-	
-	public static final int SEARCH_TYPE_CLASSIC = 1;
-	public static final int SEARCH_TYPE_BLE = 2;
-}
