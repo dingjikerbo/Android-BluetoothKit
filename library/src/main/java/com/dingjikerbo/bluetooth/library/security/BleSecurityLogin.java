19d11f99f5b13b3d544c93151b8c775bb4571317
dingjikerbo <dingjikerbo@gmail.com>
2016-01-11 19:30:36 +0800

    add so and security
    Signed-off-by: dingjikerbo <dingjikerbo@gmail.com>

---
+++ library/src/main/java/com/dingjikerbo/bluetooth/library/security/BleSecurityLogin.java
@@ -0,0 +1,290 @@
+package com.xiaomi.smarthome.device.bluetooth.security;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+
+import com.xiaomi.smarthome.bluetooth.Response;
+import com.xiaomi.smarthome.device.bluetooth.BaseManager;
+import com.xiaomi.smarthome.device.bluetooth.connect.BLEConnectManager;
+import com.xiaomi.smarthome.device.bluetooth.connect.request.Code;
+import com.xiaomi.smarthome.device.bluetooth.utils.BluetoothConstants;
+import com.xiaomi.smarthome.device.bluetooth.utils.BluetoothLog;
+import com.xiaomi.smarthome.device.bluetooth.utils.BluetoothUtils;
+import com.xiaomi.smarthome.device.bluetooth.utils.ByteUtils;
+
+import java.util.Arrays;
+import java.util.UUID;
+
+/**
+ * Created by liwentian on 2016/1/8.
+ */
+public class BleSecurityLogin extends BaseManager {
+
+    private String mDeviceMac;
+    private byte[] mToken;
+
+    private BleLoginResponse mLoginResponse;
+
+    private boolean mCanceled;
+
+    private static final int MSG_NOTIFY_TICK_TIMEOUT = 0x1;
+    private static final int MSG_NOTIFY_CONFIRM_TIMEOUT = 0x2;
+
+    private static final int SESSION_START = 0xCD43BC00;
+    private static final int SESSION_END = 0x93BFAC09;
+    private static final int SESSION_CONFIRM = 0x369A58C9;
+
+    /**
+     * token^tick形成的加密key
+     */
+    private byte[] mEncryptKey;
+
+    public BleSecurityLogin(String mac, byte[] token) {
+        mDeviceMac = mac;
+        mToken = token;
+    }
+
+    public void cancel() {
+        BluetoothLog.d("BleSecurityLogin cancel");
+        mCanceled = true;
+        unregisterBleNotifyReceiver();
+    }
+
+    public void login(BleLoginResponse response) {
+        mLoginResponse = response;
+
+        BluetoothLog.w("security login start");
+        BluetoothLog.d("connect to " + mDeviceMac);
+        BLEConnectManager.connect(mDeviceMac, mBleConnectResponse);
+    }
+
+    private final Response.BleConnectResponse mBleConnectResponse = new Response.BleConnectResponse() {
+
+        @Override
+        public void onResponse(int code, Bundle data) {
+            BluetoothLog.d("connect onResponse: " + Code.toString(code));
+
+            if (code == Code.REQUEST_SUCCESS) {
+                processStep1();
+            } else {
+                dispatchLoginResult(Code.REQUEST_FAILED);
+            }
+        }
+    };
+
+    private void processStep1() {
+        if (mCanceled) {
+            dispatchLoginResult(Code.REQUEST_CANCELED);
+            return;
+        }
+
+        BluetoothLog.d("process step 1 ...");
+
+        BLEConnectManager.notify(mDeviceMac, BluetoothConstants.MISERVICE,
+                BluetoothConstants.CHARACTER_TOKEN, mBleNotifyResponse);
+
+        registerBleNotifyReceiver();
+    }
+
+    private final Response.BleNotifyResponse mBleNotifyResponse = new Response.BleNotifyResponse() {
+        @Override
+        public void onResponse(int code, Void data) {
+            // TODO Auto-generated method stub
+            BluetoothLog.d("step 1 onResponse: " + Code.toString(code));
+
+            if (code == Code.REQUEST_SUCCESS) {
+                processStep2();
+            } else {
+                dispatchLoginResult(Code.REQUEST_FAILED);
+            }
+        }
+    };
+
+    private void processNotify(UUID service, UUID character, byte[] data) {
+        if (service.equals(BluetoothConstants.MISERVICE) && character.equals(BluetoothConstants.CHARACTER_TOKEN)) {
+            if (mHandler.hasMessages(MSG_NOTIFY_TICK_TIMEOUT)) {
+                BluetoothLog.d("login onNotify tick");
+                mHandler.removeMessages(MSG_NOTIFY_TICK_TIMEOUT);
+                processTickNotify(data);
+            } else if (mHandler
+                    .hasMessages(MSG_NOTIFY_CONFIRM_TIMEOUT)) {
+                BluetoothLog.d("login onNotify confirm");
+                mHandler.removeMessages(MSG_NOTIFY_CONFIRM_TIMEOUT);
+                processConfirmNotify(data);
+            }
+        }
+    }
+
+    private void processTickNotify(byte[] data) {
+        BluetoothLog.d("processTickNotify " + ByteUtils.byteToString(data));
+
+        if (data != null && data.length == 4) {
+            byte[] tick = BLECipher.encrypt(mToken, data);
+
+            BluetoothLog.v("processTickNotify tick = "
+                    + ByteUtils.byteToString(tick));
+
+            mEncryptKey = Arrays.copyOfRange(mToken, 0, mToken.length);
+
+            for (int i = 0; i < tick.length; i++) {
+                mEncryptKey[i] ^= tick[i];
+            }
+
+            processStep3(BLECipher.encrypt(mEncryptKey,
+                    ByteUtils.fromInt(SESSION_END)));
+
+        } else {
+            dispatchLoginResult(Code.REQUEST_FAILED);
+        }
+    }
+
+    private void processConfirmNotify(byte[] data) {
+        BluetoothLog.d("processConfirmNotify "
+                + ByteUtils.byteToString(data));
+
+        byte[] bytes = BLECipher.encrypt(mEncryptKey, data);
+
+        if (ByteUtils.byteEquals(bytes, ByteUtils.fromInt(SESSION_CONFIRM))) {
+            dispatchLoginResult(Code.REQUEST_SUCCESS);
+        } else {
+            dispatchLoginResult(Code.TOKEN_NOT_MATCHED);
+        }
+    }
+
+    private void processStep2() {
+        if (mCanceled) {
+            dispatchLoginResult(Code.REQUEST_CANCELED);
+            return;
+        }
+
+        BluetoothLog.d("process step 2");
+
+        BLEConnectManager.write(mDeviceMac, BluetoothConstants.MISERVICE,
+                BluetoothConstants.CHARACTER_EVENT,
+                ByteUtils.fromInt(SESSION_START), new Response.BleWriteResponse() {
+
+                    @Override
+                    public void onResponse(int code, Void data) {
+                        // TODO Auto-generated method stub
+                    }
+
+                });
+
+        mHandler.removeMessages(MSG_NOTIFY_TICK_TIMEOUT);
+        mHandler.sendEmptyMessageDelayed(
+                MSG_NOTIFY_TICK_TIMEOUT, BluetoothConstants.NOTIFY_TIMEOUT);
+    }
+
+    private void processStep3(byte[] bytes) {
+        if (mCanceled) {
+            dispatchLoginResult(Code.REQUEST_CANCELED);
+            return;
+        }
+
+        BluetoothLog.d("process step 3");
+
+        BLEConnectManager.write(mDeviceMac, BluetoothConstants.MISERVICE,
+                BluetoothConstants.CHARACTER_TOKEN, bytes,
+                new Response.BleWriteResponse() {
+
+                    @Override
+                    public void onResponse(int code, Void data) {
+                        // TODO Auto-generated method stub
+                    }
+
+                });
+
+        mHandler.removeMessages(MSG_NOTIFY_CONFIRM_TIMEOUT);
+        mHandler.sendEmptyMessageDelayed(
+                MSG_NOTIFY_CONFIRM_TIMEOUT, BluetoothConstants.NOTIFY_TIMEOUT);
+    }
+
+    private Handler mHandler = new Handler(Looper.getMainLooper()) {
+
+        @Override
+        public void handleMessage(Message msg) {
+            // TODO Auto-generated method stub
+            switch (msg.what) {
+                case MSG_NOTIFY_TICK_TIMEOUT:
+                    BluetoothLog.w("tick notify timeout");
+                    mHandler.removeMessages(MSG_NOTIFY_TICK_TIMEOUT);
+                    dispatchLoginResult(Code.REQUEST_FAILED);
+                    break;
+
+                case MSG_NOTIFY_CONFIRM_TIMEOUT:
+                    BluetoothLog.w("confirm notify timeout");
+                    mHandler.removeMessages(MSG_NOTIFY_CONFIRM_TIMEOUT);
+                    dispatchLoginResult(Code.TOKEN_NOT_MATCHED);
+                    break;
+            }
+        }
+
+    };
+
+    public interface BleLoginResponse extends Response.BleResponse<Void> {
+
+    }
+
+    private void dispatchLoginResult(int code) {
+        BluetoothLog.d("dispatchLoginResult " + Code.toString(code));
+
+        if (code != Code.REQUEST_SUCCESS) {
+            BLEConnectManager.disconnect(mDeviceMac);
+        }
+
+        BLEConnectManager.unnotify(mDeviceMac,
+                BluetoothConstants.MISERVICE,
+                BluetoothConstants.CHARACTER_TOKEN);
+
+        if (mLoginResponse != null) {
+            mLoginResponse.onResponse(code, null);
+        }
+
+        mLoginResponse = null;
+
+        unregisterBleNotifyReceiver();
+    }
+
+    private void registerBleNotifyReceiver() {
+        BluetoothLog.d("registerBleNotifyReceiver");
+        IntentFilter filter = new IntentFilter(BluetoothConstants.ACTION_CHARACTER_CHANGED);
+        BluetoothUtils.registerReceiver(mReceiver, filter);
+    }
+
+    private void unregisterBleNotifyReceiver() {
+        BluetoothLog.d("unregisterBleNotifyReceiver");
+        BluetoothUtils.unregisterReceiver(mReceiver);
+    }
+
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent == null) {
+                return;
+            }
+
+            String action = intent.getAction();
+
+            if (BluetoothConstants.ACTION_CHARACTER_CHANGED.equalsIgnoreCase(action)) {
+                String mac = intent.getStringExtra(BluetoothConstants.KEY_DEVICE_ADDRESS);
+
+                if (mDeviceMac.equalsIgnoreCase(mac)) {
+                    UUID service = (UUID) intent.getSerializableExtra(BluetoothConstants.KEY_SERVICE_UUID);
+                    UUID character = (UUID) intent.getSerializableExtra(BluetoothConstants.KEY_CHARACTER_UUID);
+                    byte[] value = intent.getByteArrayExtra(BluetoothConstants.KEY_CHARACTER_VALUE);
+
+                    if (service != null && character != null) {
+                        processNotify(service, character, value);
+                    }
+                }
+            }
+        }
+    };
+}
---
+++ library/src/main/java/com/dingjikerbo/bluetooth/library/security/BleSecurityRegister.java
@@ -0,0 +1,293 @@
+package com.xiaomi.smarthome.device.bluetooth.security;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+
+import com.xiaomi.smarthome.bluetooth.Response;
+import com.xiaomi.smarthome.device.bluetooth.connect.BLEConnectManager;
+import com.xiaomi.smarthome.device.bluetooth.connect.request.Code;
+import com.xiaomi.smarthome.device.bluetooth.utils.BluetoothConstants;
+import com.xiaomi.smarthome.device.bluetooth.utils.BluetoothLog;
+import com.xiaomi.smarthome.device.bluetooth.utils.BluetoothUtils;
+import com.xiaomi.smarthome.device.bluetooth.utils.ByteUtils;
+
+import java.util.UUID;
+
+/**
+ * Created by liwentian on 2016/1/8.
+ */
+public class BleSecurityRegister {
+
+    private static final int MSG_NOTIFY_TIMEOUT = 0x1;
+
+    private static final int SESSION_START = 0xDE85CA90;
+    private static final int SESSION_END = 0xFA54AB92;
+
+    private String mDeviceMac;
+    private int mProductId;
+    private BleRegisterResponse mRegisterResponse;
+
+    private boolean mCanceled;
+
+    private byte[] mToken;
+
+    public BleSecurityRegister(String mac, int pid) {
+        mDeviceMac = mac;
+        mProductId = pid;
+    }
+
+    public void register(final BleRegisterResponse response) {
+        mRegisterResponse = response;
+
+        BluetoothLog.w("BleSecurityRegister start");
+        BluetoothLog.d("Connect to " + mDeviceMac);
+        BLEConnectManager.connect(mDeviceMac, mBleConnectResponse);
+    }
+
+    private final Response.BleConnectResponse mBleConnectResponse = new Response.BleConnectResponse() {
+        @Override
+        public void onResponse(int code, Bundle data) {
+            // TODO Auto-generated method stub
+            BluetoothLog.d("connect onResponse: " + Code.toString(code));
+
+            if (code == Code.REQUEST_SUCCESS) {
+                processStep1();
+            } else {
+                dispatchRegisterResult(Code.REQUEST_FAILED);
+            }
+        }
+    };
+
+    public void cancel() {
+        BluetoothLog.d("BleSecurityRegister cancel");
+        mCanceled = true;
+        unregisterBleNotifyReceiver();
+    }
+
+    private void processStep1() {
+        if (mCanceled) {
+            dispatchRegisterResult(Code.REQUEST_CANCELED);
+            return;
+        }
+
+        BluetoothLog.d("process step 1 ...");
+
+        BLEConnectManager.write(mDeviceMac, BluetoothConstants.MISERVICE,
+                BluetoothConstants.CHARACTER_EVENT,
+                ByteUtils.fromInt(SESSION_START), new Response.BleWriteResponse() {
+
+                    @Override
+                    public void onResponse(int code, Void data) {
+                        // TODO Auto-generated method stub
+                        BluetoothLog.d("step 1 onResponse: " + Code.toString(code));
+
+                        if (code == Code.REQUEST_SUCCESS) {
+                            processStep2();
+                        } else {
+                            dispatchRegisterResult(Code.REQUEST_FAILED);
+                        }
+                    }
+
+                });
+    }
+
+    private void processStep2() {
+        if (mCanceled) {
+            dispatchRegisterResult(Code.REQUEST_CANCELED);
+            return;
+        }
+
+        BluetoothLog.d("process step 2 ...");
+
+        BLEConnectManager.notify(mDeviceMac, BluetoothConstants.MISERVICE,
+                BluetoothConstants.CHARACTER_TOKEN, mBleNotifyResponse);
+
+        registerBleNotifyReceiver();
+    }
+
+    private final Response.BleNotifyResponse mBleNotifyResponse = new Response.BleNotifyResponse() {
+        @Override
+        public void onResponse(int code, Void data) {
+            // TODO Auto-generated method stub
+            BluetoothLog.d("step 2 onResponse: " + Code.toString(code));
+
+            if (code == Code.REQUEST_SUCCESS) {
+                processStep3();
+            } else {
+                dispatchRegisterResult(Code.REQUEST_FAILED);
+            }
+        }
+
+    };
+
+    private void processNotify(UUID service, UUID character, byte[] data) {
+        if (service.equals(BluetoothConstants.MISERVICE) && character.equals(BluetoothConstants.CHARACTER_TOKEN)) {
+            if (mHandler.hasMessages(MSG_NOTIFY_TIMEOUT)) {
+                mHandler.removeMessages(MSG_NOTIFY_TIMEOUT);
+
+                byte[] key1 = BLECipher.mixA(mDeviceMac, mProductId);
+                byte[] key2 = BLECipher.mixB(mDeviceMac, mProductId);
+
+                byte[] token = BLECipher.encrypt(key2,
+                        BLECipher.encrypt(key1, data));
+
+                if (ByteUtils.byteEquals(token, mToken)) {
+                    processStep4();
+                } else {
+                    BluetoothLog.w("token not match");
+                    dispatchRegisterResult(Code.REQUEST_FAILED);
+                }
+            }
+        }
+    }
+
+    private void processStep3() {
+        if (mCanceled) {
+            dispatchRegisterResult(Code.REQUEST_CANCELED);
+            return;
+        }
+
+        BluetoothLog.d("process step 3");
+
+        mToken = BluetoothUtils.generateToken();
+
+        byte[] key1 = BLECipher.mixA(mDeviceMac, mProductId);
+
+        byte[] t1 = BLECipher.encrypt(key1, mToken);
+
+        BLEConnectManager.write(mDeviceMac, BluetoothConstants.MISERVICE,
+                BluetoothConstants.CHARACTER_TOKEN, t1, new Response.BleWriteResponse() {
+
+                    @Override
+                    public void onResponse(int code, Void data) {
+                        // TODO Auto-generated method stub
+//                        BluetoothLog.d("step 3 onResponse: " + Code.toString(code));
+//
+//                        if (code == Code.REQUEST_SUCCESS) {
+//                            mHandler.removeMessages(MSG_NOTIFY_TIMEOUT);
+//                            mHandler.sendEmptyMessageDelayed(
+//                                    MSG_NOTIFY_TIMEOUT, BluetoothConstants.NOTIFY_TIMEOUT);
+//                        } else {
+//                            dispatchRegisterResult(Code.REQUEST_FAILED);
+//                        }
+                    }
+
+                });
+
+        mHandler.removeMessages(MSG_NOTIFY_TIMEOUT);
+        mHandler.sendEmptyMessageDelayed(
+                MSG_NOTIFY_TIMEOUT, BluetoothConstants.NOTIFY_TIMEOUT);
+    }
+
+    private void processStep4() {
+        if (mCanceled) {
+            dispatchRegisterResult(Code.REQUEST_CANCELED);
+            return;
+        }
+
+        BluetoothLog.d("process step 4 ...");
+
+        byte[] flag = ByteUtils.fromInt(SESSION_END);
+        byte[] bytes = BLECipher.encrypt(mToken, flag);
+
+        BLEConnectManager.write(mDeviceMac, BluetoothConstants.MISERVICE,
+                BluetoothConstants.CHARACTER_TOKEN, bytes,
+                new Response.BleWriteResponse() {
+
+                    @Override
+                    public void onResponse(int code, Void data) {
+                        // TODO Auto-generated method stub
+                        BluetoothLog.d("step 4 onResponse: " + Code.toString(code));
+
+                        if (code == Code.REQUEST_SUCCESS) {
+                            dispatchRegisterResult(Code.REQUEST_SUCCESS);
+                        } else {
+                            dispatchRegisterResult(Code.REQUEST_FAILED);
+                        }
+                    }
+
+                });
+    }
+
+    private Handler mHandler = new Handler(Looper.getMainLooper()) {
+
+        @Override
+        public void handleMessage(Message msg) {
+            // TODO Auto-generated method stub
+            switch (msg.what) {
+                case MSG_NOTIFY_TIMEOUT:
+                    BluetoothLog.w("notify timeout");
+                    dispatchRegisterResult(Code.REQUEST_FAILED);
+                    break;
+            }
+        }
+
+    };
+
+    public interface BleRegisterResponse extends Response.BleResponse<byte[]> {
+
+    };
+
+    private void dispatchRegisterResult(int code) {
+        BluetoothLog.d("dispatchRegisterResult " + Code.toString(code));
+
+        if (code != Code.REQUEST_SUCCESS) {
+            BLEConnectManager.disconnect(mDeviceMac);
+        }
+
+        BLEConnectManager.unnotify(mDeviceMac,
+                BluetoothConstants.MISERVICE,
+                BluetoothConstants.CHARACTER_TOKEN);
+
+        if (mRegisterResponse != null) {
+            mRegisterResponse.onResponse(code, mToken);
+        }
+
+        mRegisterResponse = null;
+
+        unregisterBleNotifyReceiver();
+    }
+
+    private void registerBleNotifyReceiver() {
+        BluetoothLog.d("registerBleNotifyReceiver");
+        IntentFilter filter = new IntentFilter(BluetoothConstants.ACTION_CHARACTER_CHANGED);
+        BluetoothUtils.registerReceiver(mReceiver, filter);
+    }
+
+    private void unregisterBleNotifyReceiver() {
+        BluetoothLog.d("unregisterBleNotifyReceiver");
+        BluetoothUtils.unregisterReceiver(mReceiver);
+    }
+
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent == null) {
+                return;
+            }
+
+            String action = intent.getAction();
+
+            if (BluetoothConstants.ACTION_CHARACTER_CHANGED.equalsIgnoreCase(action)) {
+                String mac = intent.getStringExtra(BluetoothConstants.KEY_DEVICE_ADDRESS);
+
+                if (mDeviceMac.equalsIgnoreCase(mac)) {
+                    UUID service = (UUID) intent.getSerializableExtra(BluetoothConstants.KEY_SERVICE_UUID);
+                    UUID character = (UUID) intent.getSerializableExtra(BluetoothConstants.KEY_CHARACTER_UUID);
+                    byte[] value = intent.getByteArrayExtra(BluetoothConstants.KEY_CHARACTER_VALUE);
+
+                    if (service != null && character != null) {
+                        processNotify(service, character, value);
+                    }
+                }
+            }
+        }
+    };
+}
