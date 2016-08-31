cf32a86d5a64e59537468fec1f546085abbea4cf
liwentian <liwentian@xiaomi.com>
2016-08-31 20:23:51 +0800

    update

--- app/src/main/java/com/inuker/bluetooth/MainActivity.java
+++ app/src/main/java/com/inuker/bluetooth/MainActivity.java
@@ -153,7 +153,6 @@ public class MainActivity extends Activity {
             @Override
             public void onResponse(int code) {
                 if (code == REQUEST_SUCCESS) {
-                    BluetoothGatt.GATT_FAILURE
                 }
             }
         });
